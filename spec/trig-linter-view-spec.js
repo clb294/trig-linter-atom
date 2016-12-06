fea96a539828904699fd39d661beb8e91b3dc324
Chris Borodic <cborodic@cambridgesemantics.com>
2016-12-06 17:30:10 -0500

    first commit of trig-linter

---
+++ spec/trig-linter-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TrigLinterView from '../lib/trig-linter-view';
+
+describe('TrigLinterView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
